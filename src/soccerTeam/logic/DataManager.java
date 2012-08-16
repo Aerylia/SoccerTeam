9c1c5848ce1b64a155c8783c07d309ab7eca281f
Arianne Meijer <ariannemeijer@gmail.com>
2012-08-16 17:02:46 +0200

    moved Datamanager

--- src/soccerTeam/logic/DataManager.java
+++ src/soccerTeam/logic/DataManager.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package soccerTeam.control;
+package soccerTeam.logic;
 
 import java.io.*;
 import soccerTeam.logic.DataPortal;
